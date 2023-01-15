5154f4c8fe320637e59097c0223f1491ac1bb6e3
Peter DeMarco <yinrunning@gmail.com>
2023-01-15 17:29:44 -0600

    Fix output issues

--- src/TestRunner.php
+++ src/TestRunner.php
@@ -14,65 +14,68 @@ class TestRunner
 
     public static function coverageClover(Event $event): void
     {
-    	static::run([
-    		"vendor/bin/phpunit",
-    		"-dmemory_limit=-1",
-    		"-dpcov.directory=./",
-    		"--coverage-clover=coverage.xml",
-    	]);
+        static::run([
+            "vendor/bin/phpunit",
+            "-dmemory_limit=-1",
+            "--colors=always",
+            "-dpcov.directory=./",
+            "--coverage-clover=coverage.xml",
+        ]);
     }
 
     public static function coverageFull(Event $event): void
     {
-    	static::run([
-    		"vendor/bin/phpunit",
-    		"-dmemory_limit=-1",
-    		"-dpcov.directory=./",
-    		"--coverage-html",
-    		"coverage",
-		]);
+        static::run([
+            "vendor/bin/phpunit",
+            "-dmemory_limit=-1",
+            "--colors=always",
+            "-dpcov.directory=./",
+            "--coverage-html",
+            "coverage",
+        ]);
 
-		static::run([
-    		"open",
-    		"coverage/index.html",
-    	]);
+        static::run([
+            "open",
+            "coverage/index.html",
+        ]);
     }
 
     public static function coverageText(Event $event): void
     {
-    	static::run([
-    		"vendor/bin/phpunit",
-    		"-dmemory_limit=-1",
-    		"-dpcov.directory=./",
-    		"--coverage-text",
-    		"--colors=never",
-    	]);
+        static::run([
+            "vendor/bin/phpunit",
+            "-dmemory_limit=-1",
+            "-dpcov.directory=./",
+            "--coverage-text",
+            "--colors=never",
+        ]);
     }
 
     public static function test(Event $event): void
     {
-    	static::run([
-    		"vendor/bin/phpunit",
-    		"--colors",
-    		"-dmemory_limit=-1",
-    	]);
-    }
-
-	public static function filterTests(Event $event): void
-    {   
-    	$filter = static::extract($event, 0, 'You must supply a string to filter tests on');
-
         static::run([
-			"vendor/bin/phpunit",
-			"--filter",
-			$filter,
+            "vendor/bin/phpunit",
+            "--colors=always",
+            "-dmemory_limit=-1",
         ]);
     }
 
-	public static function repeatTest(Event $event): void
+    public static function filterTests(Event $event): void
     {   
-    	$times = $event->getArguments()[0] ?? '';
-    	$filter = $event->getArguments()[1] ?? '';
+        $filter = static::extract($event, 0, 'You must supply a string to filter tests on');
+
+        static::run([
+            "vendor/bin/phpunit",
+            "--colors=always",
+            "--filter",
+            $filter,
+        ]);
+    }
+
+    public static function repeatTest(Event $event): void
+    {   
+        $times = $event->getArguments()[0] ?? '';
+        $filter = $event->getArguments()[1] ?? '';
 
         if (empty($times)) {
             throw new Exception('You must supply a number of times to run the filtered tests');
@@ -82,18 +85,19 @@ class TestRunner
             throw new Exception('You must supply a string to filter tests on');
         }
 
-		static::run([
-			"vendor/bin/phpunit",
-			"--repeat",
-			$times,
-			"--filter",
-			$filter,
-		]);
+        static::run([
+            "vendor/bin/phpunit",
+            "--colors=always",
+            "--repeat",
+            $times,
+            "--filter",
+            $filter,
+        ]);
     }
 
     protected static function extract(Event $event, int $index, string $description): mixed
     {
-    	$argument = $event->getArguments()[0] ?? null;
+        $argument = $event->getArguments()[0] ?? null;
 
         if (is_null($argument)) {
             throw new Exception('You must supply a string to filter tests on');
@@ -104,14 +108,10 @@ class TestRunner
 
     protected static function run(array $arguments): void
     {
-    	$process = new Process($arguments);
+        $process = new Process($arguments);
 
-    	$process->run();
-
-		if (! $process->isSuccessful()) {
-		    throw new ProcessFailedException($process);
-		}
-
-		echo $process->getOutput();
+        $process->run(function ($type, $buffer) {
+            echo $buffer;
+        });
     }
 }
